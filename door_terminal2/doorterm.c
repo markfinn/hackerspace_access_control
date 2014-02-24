15b71d0d3d36e054e62f28e48c5d2c8d7649f36c
Mark Finn <mfinn@fedex.com>
2014-02-24 10:22:39 -0600

    start new door term

---
+++ door_terminal2/doorterm.c
@@ -0,0 +1,350 @@
+/*************************************************************************
+Title:    MRBus Door Terminal
+Authors:  Mark Finn <mark@mfinn.net>
+License:  GNU General Public License v3
+
+LICENSE:
+    Copyright (C) 2014 Mark Finn
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+*************************************************************************/
+
+#include <stdlib.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <avr/eeprom.h>
+#include <avr/wdt.h>
+#include <util/delay.h>
+
+#include "mrbus.h"
+
+uint8_t mrbus_dev_addr = 0;
+uint8_t pkt_count = 0;
+
+
+#define DD_SS     PINB2
+#define DD_MOSI     PINB3
+#define DD_MISO     PINB4
+#define DD_SCK     PINB5
+#define DDR_SPI    DDRB
+
+
+// ******** Start 100 Hz Timer - Very Accurate Version
+
+volatile uint8_t ticks;
+volatile uint16_t decisecs=0;
+volatile uint16_t update_decisecs=10;
+
+void initialize100HzTimer(void)
+{
+	// Set up timer 1 for 100Hz interrupts
+	TCCR1A = 0;
+	TCCR1B = _BV(CS11) | _BV(CS10);
+	TCCR1C = 0;
+	TIMSK1 = _BV(TOIE1);
+	ticks = 0;
+	decisecs = 0;
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	TCNT1 += 0xF3CB;
+	if (++ticks >= 10)
+	{
+		ticks = 0;
+		decisecs++;
+	}
+}
+
+
+ISR(SPI_STC_vect)
+{
+idle
+
+
+}
+
+
+void PktHandler(void)
+{
+	uint16_t crc = 0;
+	uint8_t i;
+	uint8_t rxBuffer[MRBUS_BUFFER_SIZE];
+	uint8_t txBuffer[MRBUS_BUFFER_SIZE];
+
+	if (0 == mrbusPktQueuePop(&mrbusRxQueue, rxBuffer, sizeof(rxBuffer)))
+		return;
+
+
+	//*************** PACKET FILTER ***************
+	// Loopback Test - did we send it?  If so, we probably want to ignore it
+	if (rxBuffer[MRBUS_PKT_SRC] == mrbus_dev_addr) 
+		goto	PktIgnore;
+
+	// Destination Test - is this for us or broadcast?  If not, ignore
+	if (0xFF != rxBuffer[MRBUS_PKT_DEST] && mrbus_dev_addr != rxBuffer[MRBUS_PKT_DEST]) 
+		goto	PktIgnore;
+	
+	// CRC16 Test - is the packet intact?
+	for(i=0; i<rxBuffer[MRBUS_PKT_LEN]; i++)
+	{
+		if ((i != MRBUS_PKT_CRC_H) && (i != MRBUS_PKT_CRC_L)) 
+			crc = mrbusCRC16Update(crc, rxBuffer[i]);
+	}
+	if ((UINT16_HIGH_BYTE(crc) != rxBuffer[MRBUS_PKT_CRC_H]) || (UINT16_LOW_BYTE(crc) != rxBuffer[MRBUS_PKT_CRC_L]))
+		goto	PktIgnore;
+		
+	//*************** END PACKET FILTER ***************
+
+
+	//*************** PACKET HANDLER - PROCESS HERE ***************
+
+	// Just smash the transmit buffer if we happen to see a packet directed to us
+	// that requires an immediate response
+	//
+	// If we're in here, then either we're transmitting, then we can't be 
+	// receiving from someone else, or we failed to transmit whatever we were sending
+	// and we're waiting to try again.  Either way, we're not going to corrupt an
+	// in-progress transmission.
+	//
+	// All other non-immediate transmissions (such as scheduled status updates)
+	// should be sent out of the main loop so that they don't step on things in
+	// the transmit buffer
+	
+	if ('A' == rxBuffer[MRBUS_PKT_TYPE])
+	{
+		// PING packet
+		txBuffer[MRBUS_PKT_DEST] = rxBuffer[MRBUS_PKT_SRC];
+		txBuffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+		txBuffer[MRBUS_PKT_LEN] = 6;
+		txBuffer[MRBUS_PKT_TYPE] = 'a';
+		mrbusPktQueuePush(&mrbusTxQueue, txBuffer, txBuffer[MRBUS_PKT_LEN]);
+		goto PktIgnore;
+	} 
+	else if ('W' == rxBuffer[MRBUS_PKT_TYPE]) 
+	{
+		// EEPROM WRITE Packet
+		txBuffer[MRBUS_PKT_DEST] = rxBuffer[MRBUS_PKT_SRC];
+		txBuffer[MRBUS_PKT_LEN] = 8;			
+		txBuffer[MRBUS_PKT_TYPE] = 'w';
+		eeprom_write_byte((uint8_t*)(uint16_t)rxBuffer[6], rxBuffer[7]);
+		txBuffer[6] = rxBuffer[6];
+		txBuffer[7] = rxBuffer[7];
+		if (MRBUS_EE_DEVICE_ADDR == rxBuffer[6])
+			mrbus_dev_addr = eeprom_read_byte((uint8_t*)MRBUS_EE_DEVICE_ADDR);
+		txBuffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+		mrbusPktQueuePush(&mrbusTxQueue, txBuffer, txBuffer[MRBUS_PKT_LEN]);
+		goto PktIgnore;	
+	}
+	else if ('R' == rxBuffer[MRBUS_PKT_TYPE]) 
+	{
+		// EEPROM READ Packet
+		txBuffer[MRBUS_PKT_DEST] = rxBuffer[MRBUS_PKT_SRC];
+		txBuffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+		txBuffer[MRBUS_PKT_LEN] = 8;			
+		txBuffer[MRBUS_PKT_TYPE] = 'r';
+		txBuffer[6] = rxBuffer[6];
+		txBuffer[7] = eeprom_read_byte((uint8_t*)(uint16_t)rxBuffer[6]);			
+		mrbusPktQueuePush(&mrbusTxQueue, txBuffer, txBuffer[MRBUS_PKT_LEN]);
+		goto PktIgnore;
+	}
+	else if ('V' == rxBuffer[MRBUS_PKT_TYPE]) 
+	{
+		// Version
+		txBuffer[MRBUS_PKT_DEST] = rxBuffer[MRBUS_PKT_SRC];
+		txBuffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+		txBuffer[MRBUS_PKT_LEN] = 16;
+		txBuffer[MRBUS_PKT_TYPE] = 'v';
+		txBuffer[6]  = MRBUS_VERSION_WIRED;
+		txBuffer[7]  = 0; // Software Revision
+		txBuffer[8]  = 0; // Software Revision
+		txBuffer[9]  = 0; // Software Revision
+		txBuffer[10]  = 0; // Hardware Major Revision
+		txBuffer[11]  = 0; // Hardware Minor Revision
+		txBuffer[12] = 'T';
+		txBuffer[13] = 'M';
+		txBuffer[14] = 'P';
+		txBuffer[15] = 'L';
+		mrbusPktQueuePush(&mrbusTxQueue, txBuffer, txBuffer[MRBUS_PKT_LEN]);
+		goto PktIgnore;
+	}
+	else if ('X' == rxBuffer[MRBUS_PKT_TYPE]) 
+	{
+		// Reset
+		cli();
+		wdt_reset();
+		MCUSR &= ~(_BV(WDRF));
+		WDTCSR |= _BV(WDE) | _BV(WDCE);
+		WDTCSR = _BV(WDE);
+		while(1);  // Force a watchdog reset
+		sei();
+	}
+
+	// FIXME:  Insert code here to handle incoming packets specific
+	// to the device.
+
+	//*************** END PACKET HANDLER  ***************
+
+	
+	//*************** RECEIVE CLEANUP ***************
+PktIgnore:
+	// Yes, I hate gotos as well, but sometimes they're a really handy and efficient
+	// way to jump to a common block of cleanup code at the end of a function 
+
+	// This section resets anything that needs to be reset in order to allow us to receive
+	// another packet.  Typically, that's just clearing the MRBUS_RX_PKT_READY flag to 
+	// indicate to the core library that the mrbus_rx_buffer is clear.
+	return;	
+}
+
+void init(void)
+{
+	// Set MOSI and SCK output, all others input
+	DDR_SPI = (1<<DD_MOSI)|(1<<DD_SCK);
+	// Enable SPI, Master, set clock rate fck/16
+	SPCR = (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+	
+	// Clear watchdog (in the case of an 'X' packet reset)
+	MCUSR = 0;
+#ifdef ENABLE_WATCHDOG
+	// If you don't want the watchdog to do system reset, remove this chunk of code
+	wdt_reset();
+	WDTCSR |= _BV(WDE) | _BV(WDCE);
+	WDTCSR = _BV(WDE) | _BV(WDP2) | _BV(WDP1); // Set the WDT to system reset and 1s timeout
+	wdt_reset();
+#else
+	wdt_reset();
+	wdt_disable();
+#endif	
+
+	pkt_count = 0;
+
+	// Initialize MRBus address from EEPROM
+	mrbus_dev_addr = eeprom_read_byte((uint8_t*)MRBUS_EE_DEVICE_ADDR);
+	// Bogus addresses, fix to default address
+	if (0xFF == mrbus_dev_addr || 0x00 == mrbus_dev_addr)
+	{
+		mrbus_dev_addr = 0x03;
+		eeprom_write_byte((uint8_t*)MRBUS_EE_DEVICE_ADDR, mrbus_dev_addr);
+	}
+	
+	update_decisecs = (uint16_t)eeprom_read_byte((uint8_t*)MRBUS_EE_DEVICE_UPDATE_L) 
+		| (((uint16_t)eeprom_read_byte((uint8_t*)MRBUS_EE_DEVICE_UPDATE_H)) << 8);
+
+	// This line assures that update_decisecs is at least 1
+	update_decisecs = max(1, update_decisecs);
+	
+	// FIXME: This line assures that update_decisecs is 2 seconds or less
+	// You probably don't want this, but it prevents new developers from wondering
+	// why their new node doesn't transmit (uninitialized eeprom will make the update
+	// interval 64k decisecs, or about 110 hours)  You'll probably want to make this
+	// something more sane for your node type, or remove it entirely.
+	update_decisecs = min(20, update_decisecs);
+
+
+/*
+// Uncomment this block to set up the ADC to continuously monitor the bus voltage using a 3:1 divider tied into the ADC7 input
+// You also need to uncomment the ADC ISR near the top of the file
+	// Setup ADC
+	ADMUX  = 0x47;  // AVCC reference; ADC7 input
+	ADCSRA = _BV(ADATE) | _BV(ADIF) | _BV(ADPS2) | _BV(ADPS1); // 128 prescaler
+	ADCSRB = 0x00;
+	DIDR0  = 0x00;  // No digitals were harmed in the making of this ADC
+
+	busVoltage = 0;
+	ADCSRA |= _BV(ADEN) | _BV(ADSC) | _BV(ADIE) | _BV(ADIF);
+*/
+}
+
+#define MRBUS_TX_BUFFER_DEPTH 4
+#define MRBUS_RX_BUFFER_DEPTH 8
+
+MRBusPacket mrbusTxPktBufferArray[MRBUS_TX_BUFFER_DEPTH];
+MRBusPacket mrbusRxPktBufferArray[MRBUS_RX_BUFFER_DEPTH];
+
+int main(void)
+{
+	// Application initialization
+	init();
+
+	// Initialize a 100 Hz timer.  See the definition for this function - you can
+	// remove it if you don't use it.
+	initialize100HzTimer();
+
+	// Initialize MRBus core
+	mrbusPktQueueInitialize(&mrbusTxQueue, mrbusTxPktBufferArray, MRBUS_TX_BUFFER_DEPTH);
+	mrbusPktQueueInitialize(&mrbusRxQueue, mrbusRxPktBufferArray, MRBUS_RX_BUFFER_DEPTH);
+	mrbusInit();
+
+	sei();	
+
+	while (1)
+	{
+		wdt_reset();
+
+#ifdef MRBEE
+		mrbeePoll();
+#endif
+		// Handle any packets that may have come in
+		if (mrbusPktQueueDepth(&mrbusRxQueue))
+			PktHandler();
+			
+		// FIXME: Do any module-specific behaviours here in the loop.
+		
+		if (decisecs >= update_decisecs && !(mrbusPktQueueFull(&mrbusTxQueue)))
+		{
+			uint8_t txBuffer[MRBUS_BUFFER_SIZE];
+
+			txBuffer[MRBUS_PKT_SRC] = mrbus_dev_addr;
+			txBuffer[MRBUS_PKT_DEST] = 0xFF;
+			txBuffer[MRBUS_PKT_LEN] = 9;
+			txBuffer[5] = 'S';
+			txBuffer[6] = 'h';
+			txBuffer[7] = 'i';
+			txBuffer[8] = pkt_count++;
+			mrbusPktQueuePush(&mrbusTxQueue, txBuffer, txBuffer[MRBUS_PKT_LEN]);
+			decisecs = 0;
+		}	
+
+		if (mrbusPktQueueDepth(&mrbusTxQueue))
+		{
+			uint8_t fail = mrbusTransmit();
+
+			// If we're here, we failed to start transmission due to somebody else transmitting
+			// Given that our transmit buffer is full, priority one should be getting that data onto
+			// the bus so we can start using our tx buffer again.  So we stay in the while loop, trying
+			// to get bus time.
+
+			// We want to wait 20ms before we try a retransmit to avoid hammering the bus
+			// Because MRBus has a minimum packet size of 6 bytes @ 57.6kbps,
+			// need to check roughly every millisecond to see if we have a new packet
+			// so that we don't miss things we're receiving while waiting to transmit
+			if (fail)
+			{
+
+#ifndef MRBEE
+				uint8_t bus_countdown = 20;
+				while (bus_countdown-- > 0 && !mrbusIsBusIdle())
+				{
+					wdt_reset();
+					_delay_ms(1);
+					if (mrbusPktQueueDepth(&mrbusRxQueue))
+						PktHandler();
+				}
+#endif
+			}
+		}
+	}
+}
+
+
+
