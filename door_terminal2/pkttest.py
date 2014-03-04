a42328c8e3f2f18ae13cd9d3275824bed4cbc906
Mark Finn <mark@mfinn.net>
2014-03-03 21:04:22 -0600

    start of protocol with simulator.  need to re-do as threaded.

---
+++ door_terminal2/pkttest.py
@@ -0,0 +1,234 @@
+import sys
+sys.path.insert(0, '../../mrbus_bootloader')
+import mrbus
+from collections import deque
+
+
+class Evobj(object):
+  def __init__(self, sim=None):
+    self.e=[]
+    if sim:
+      sim.add(self)
+
+  def getNextEventTime(self):
+    if self.e:
+      return self.e[0]
+
+  def doEvent(self, n):
+    assert self.e[0][0] == n
+    self.e[0][1](self)
+    self.e=self.e[0][1:]
+
+  def setsim(self, sim):
+    self.sim=sim
+
+
+
+class Sim(object):
+  def __init__(self):
+    self.objs=[]
+    self.t=0
+
+  def add(self, o):
+    self.objs.append(o)
+    o.setsim(self)
+
+  def run(self):
+    while True:
+      soonest=None
+      for o in self.objs:
+        n = o.getNextEventTime()
+        if n != None and (soonest == None or n < soonest[0]):
+          soonest = n, o
+      if soonest:
+        if self.t < soonest[0]:
+          self.t = soonest[0]+.01
+        else:
+          soonest[0]+=.01
+        soonest[1].doEvent(soonest[0])
+      else:
+        assert 0
+      
+        
+  def time(self):
+    self.t+=.0001
+    return self.t
+
+  def sleep(self, t):
+    assert 0
+
+
+
+class FakeBus(Evobj):
+  def __init__(self, sim=None):
+    Evobj.__init__(self, sim)
+    self.nodes=[]
+
+  def addnode(self, mrbs):
+    self.nodes.append(mrbs)
+
+  def time(self):
+    return self.sim.time()
+
+  def sleep(self, t):
+    self.sim.sleep(t)
+
+  def tx(self, dest, src, data):
+    p = mrbus.packet(dest, src, data[0], data[1:])
+    for n in self.nodes:
+      n.pktlst.append(p)
+
+
+
+class FakeMRBusSimple(Evobj):
+  def __init__(self, fakeBus, addr=None, logfile=None, logall=False, extra=False):
+    Evobj.__init__(self)
+
+
+    self.timeout=.1
+
+    self.fakeBus=fakeBus
+    self.pktlst=deque()
+    fakeBus.addnode(self)
+
+    self.logfile=logfile
+    self.logall=logall
+    self.log(0, "instantiated FakeMRBusSimple from FAKE")
+
+    self.addr=addr
+
+  def name(self):
+    return 'FAKE'
+
+  def setTimeout(self, to):
+    self.timeout=to
+
+  def getpkt(self):
+    if self.pktlst:
+      return self.pktlst.popleft()
+
+    if self.timeout == 0:
+      return None
+
+    assert 0
+
+  def sendpkt(self, dest, data, src=None):
+    if src == None:
+      src = self.addr
+    z=[]
+    for d in data:
+      if type(d) == str:
+        d=ord(d)
+      z.append(d)
+#    self.log(0, '>>>'+s)
+    self.fakeBus.tx(dest, src, data)
+
+
+
+
+  def log(self, error, msg):
+    if not self.logfile:
+      return
+    if not (error or self.logall):
+      return
+    if error:
+      s="Error:"
+    else:
+      s="  log:"
+    self.logfile.write(s+repr(msg)+'\n')
+
+  def time(self):
+    return self.fakeBus.time()
+
+  def sleep(self, t):
+    self.fakeBus.sleep(t)
+
+
+
+
+
+class RandUser(Evobj):
+  def __init__(self, send, recv):
+    Evobj.__init__(self)
+    self.send = send
+    self.recv = recv
+
+
+
+
+
+class RDB(Evobj):
+  class RDB_Connection():
+    STATE_CLOSED     = 0
+    STATE_LISTEN     = 1
+    STATE_SYN_SENT   = 2
+    STATE_SYN_RCVD   = 3
+    STATE_OPEN       = 4
+    STATE_CLOSE_WAIT = 5
+
+    def __init__(self):
+      self.state = self.STATE_CLOSED
+      
+    
+  def __init__(self, node):
+    self.node = node
+    self.connections = {} 
+
+
+  def listen(self):
+    def do(p):
+      print p
+    hint = self.node.install(do, -1)
+    
+  def open(self, dest):
+    assert dest not in self.connections
+    c = self.RDB_Connection()
+    self.connections[dest]=c
+    c.state = self.RDB_Connection.STATE_SYN_SENT
+    self.node.sendpkt([128, 0])
+
+
+
+
+
+
+
+
+def test():
+
+
+  sim = Sim()
+
+  net = FakeBus(sim)
+
+  p1= FakeMRBusSimple(net)
+  p2= FakeMRBusSimple(net)
+
+  mrb1 = mrbus.mrbus(p1, addr=1)#, logall=True, logfile=sys.stdout, extra=True)
+  mrb2 = mrbus.mrbus(p2, addr=2)#, logall=True, logfile=sys.stdout, extra=True)
+
+  n2 = mrb1.getnode(2)
+  n1 = mrb2.getnode(1)
+
+  r1=RDB(n1)
+  r2=RDB(n2)
+
+
+  r2.listen()
+  n2.pump()
+  r1.open(2)
+  n1.pump()
+
+  sim.run()
+
+  rnd1=RandUser(n1, n2)
+  sim.add(rnd1)
+  rnd2=RandUser(n2, n1)
+  sim.add(rnd2)
+
+
+
+test()
+
+
+
