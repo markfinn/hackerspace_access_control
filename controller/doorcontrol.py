3a54c62f1d230c08fead36d8daa2de0611838569
Mark Finn <mark@mfinn.net>
2015-07-18 11:35:32 -0500

    add some status reporting

--- controller/doorcontrol.py
+++ controller/doorcontrol.py
@@ -4,6 +4,7 @@ import sys
 import argparse
 import os
 import struct
+import datetime
 
 sys.path.insert(0, '../pymrbus')
 import mrbus
@@ -57,7 +58,6 @@ def run(pintest):
 
   ns=mrb.getnode(0x10)
   nk=mrb.getnode(0x5C)
-  openit=None
 
 
 
@@ -66,11 +66,18 @@ def run(pintest):
 #      print 'authreq:', p
 
       uniq, strike, id, pin =  p.data[0], p.data[1], p.data[2], PinLenDecode(sum(p.data[3+i]<<(8*i) for i in xrange(4)))
+      tm = datetime.datetime.now()
+       
       if pintest(id, pin):
         #ignores requested strike...
+        print '%s ID: %02u Unlock'%(tm.isoformat(sep=' '), id)
+        sys.stdout.flush()
         nk.sendpkt(['o', uniq, 1, 100])
         ns.sendpkt(['C', 100, 1])
+
       else:
+        print '%s ID: %02u Fail'%(tm.isoformat(sep=' '), id)
+        sys.stdout.flush()
         nk.sendpkt(['o', uniq, 0, 100])
         #cache the result for a while and by addr/uniq
         #set up rety handlers
@@ -79,12 +86,43 @@ def run(pintest):
       return True #eat packet
     return False #dont eat packet
 
-  nk.install(authreqhandler, 0)
+
+  def strikeStatushandler(p, staticarr=[None]):
+    if p.cmd==ord('S'):
+      tm = datetime.datetime.now()
+      dooropen, dooropenhold, booted = (p.data[0]&(1<<i)!=0 for i in xrange(3))
+      unlocked = p.data[1]!=0
+      if (dooropenhold and not dooropen) or booted:
+        ns.sendpkt(['Z'])
+      if (unlocked, dooropen, dooropenhold, booted) != staticarr[0]:
+        s='%s door status change:'%(tm.isoformat(sep=' '))
+        if unlocked:
+          s+='unlocked, '
+        if dooropen:
+          s+='door open, '
+        if dooropenhold:
+          s+='door was opened, '
+        if booted:
+          s+='strike rebooted'
+        print s
+        sys.stdout.flush()
+        staticarr[0] = (unlocked, dooropen, dooropenhold, booted)
+      return True #eat packet
+    return False #dont eat packet
+
 
 
   mrb.pumpout()
+  nk.install(authreqhandler, 0)
+  ns.install(strikeStatushandler, 0)
+  tm = datetime.datetime.now()
+  print '%s start'%(tm.isoformat(sep=' '))
+  sys.stdout.flush()
   while 1:
     mrb.pump()
+  tm = datetime.datetime.now()
+  print '%s exit'%(tm.isoformat(sep=' '))
+  sys.stdout.flush()
  
 #5c->* O [uinq], 10, id,pin4enc
 #FE->5c o uinq,0-fail, 1-success, 100
