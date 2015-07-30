f6b35b1a01f5821991ce04a09ac30f451e9f936b
Mark Finn <mark@mfinn.net>
2015-07-29 23:59:47 -0500

    dev/tty doesn't exist when run from init

--- controller/doorcontrol.py
+++ controller/doorcontrol.py
@@ -138,7 +138,7 @@ class App():
   def __init__(self):
     self.stdin_path = '/dev/null'
     self.stdout_path = '/dev/null'
-    self.stderr_path = '/dev/tty'
+    self.stderr_path = '/dev/null'
     self.pidfile_path =  '/var/run/doorcontrol.pid'
     self.pidfile_timeout = 5
   def run(self):
